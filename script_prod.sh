97f88c2aad4c0783780f7261693d23c5a7eb1ef0
Ciro <ciro.napolitano87@gmail.com>
2021-10-18 11:29:41 +0200

    modificati script per il deploy

--- script_prod.sh
+++ script_prod.sh
@@ -4,5 +4,6 @@ rm -r -f deploy
 mkdir deploy
 cp -R controllers deploy/
 cp -R classes deploy/
+cp -R templates_twig deploy/
 scp -r deploy root@tecneco.3d0.it:/home/catalogotecneco/public_html/modules/b2b
 rm -r -f deploy
--- script_staging.sh
+++ script_staging.sh
@@ -3,5 +3,6 @@ echo "STAGING"
 mkdir deploy
 cp -R controllers deploy/
 cp -R classes deploy/
+cp -R templates_twig deploy/
 scp -r deploy root@tecneco.3d0.it:/home/b2bbe/public_html/modules/b2b
 rm -r -f deploy
