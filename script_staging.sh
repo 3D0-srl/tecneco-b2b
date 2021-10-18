a1a61754c7464ad585b191135c6b3e5b5cafda48
Ciro <ciro.napolitano87@gmail.com>
2021-10-18 11:35:03 +0200

    modificati script per il deploy

--- script_staging.sh
+++ script_staging.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "STAGING"
+rm -r -f deploy
 mkdir deploy
 cp -R controllers deploy/
 cp -R classes deploy/
 cp -R templates_twig deploy/
-scp -r deploy root@tecneco.3d0.it:/home/b2bbe/public_html/modules/b2b
+scp -r deploy/* root@tecneco.3d0.it:/home/b2bbe/public_html/modules/b2b
 rm -r -f deploy
